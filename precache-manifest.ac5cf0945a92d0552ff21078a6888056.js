c740df94269abd9b1e966b52b0872ffc871ba5fe
James Bates <j4bates@gmail.com>
2019-05-15 23:50:39 -0400

    Updates

---
+++ precache-manifest.ac5cf0945a92d0552ff21078a6888056.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "184e16840cd7a4664f850a71a910a4ef",
+    "url": "/my-site/index.html"
+  },
+  {
+    "revision": "2e2afd3cf03bed3f95e5",
+    "url": "/my-site/static/css/main.7c8a8f4a.chunk.css"
+  },
+  {
+    "revision": "2ff10ca9bf38a5ed61bb",
+    "url": "/my-site/static/js/2.31e4570f.chunk.js"
+  },
+  {
+    "revision": "2e2afd3cf03bed3f95e5",
+    "url": "/my-site/static/js/main.fd895514.chunk.js"
+  },
+  {
+    "revision": "32bfdd9f18f755e00cad",
+    "url": "/my-site/static/js/runtime~main.0ae2f0e4.js"
+  }
+]);
